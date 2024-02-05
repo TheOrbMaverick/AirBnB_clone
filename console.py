fb14ace5b8ed71759450b3c27d2978ca50fc4486
TheOrbMaverick <enangita@gmail.com>
2024-02-05 17:18:48 +0100

    checking pycodestyle

--- console.py
+++ console.py
@@ -9,25 +9,25 @@ import cmd
 
 class HBNBCommand(cmd.Cmd):
 
-	"""
-	HBNBCommand class for handling commands in the HBNB application.
+    """
+    HBNBCommand class for handling commands in the HBNB application.
 
-	Attributes:
-	- prompt (str): The prompt displayed to the user.
+    Attributes:
+    - prompt (str): The prompt displayed to the user.
 
-	Methods:
-	- do_quit(arg): Exits the program.
-	- do_exit(arg): Alias for the quit command.
-	"""
+    Methods:
+    - do_quit(arg): Exits the program.
+    - do_exit(arg): Alias for the quit command.
+    """
 
-	prompt = "(hbnb) "
+    prompt = "(hbnb) "
 
-	def do_quit(self, arg):
-		"""Quit command to exit the program"""
-		return True
-	
-	#aliasing the command
-	do_exit = do_quit
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
+
+    #aliasing the command
+    do_exit = do_quit
 
 if __name__ == '__main__':
-	HBNBCommand().cmdloop()
+    HBNBCommand().cmdloop()
