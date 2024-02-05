2bd2d3fe7b2c42d6aa988b2817ce7cbbab795655
TheOrbMaverick <enangita@gmail.com>
2024-02-05 16:16:09 +0100

    first console commands

--- console.py
+++ console.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+
+import cmd
+
+class HBNBCommand(cmd.Cmd):
+	prompt = "(hbnb) "
+
+	def do_quit(self, arg):
+		"""Quit command to exit the program"""
+		return True
+	
+	#aliasing the command
+	do_exit = do_quit
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
